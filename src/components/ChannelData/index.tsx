8df4c24194815d29661765c84449cffb7043cb7c
gheysiell <gheysiellfarukzinn@hotmail.com>
2022-08-02 06:45:21 -0400

    first commit

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,52 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Gheysiell Santana"
+            date="02/08/2022"
+            content="Message here"
+          />
+        ))}
+
+        <ChannelMessage
+          author="João Silva"
+          date="02/08/2022"
+          content={
+            <>
+              <Mention>@Gheysiell Santana</Mention>, pode me ajudar em um projeto
+              por favor?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversarem #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
